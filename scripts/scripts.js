162f4167e0d737395c06304f6c5249166780230c
Mary Dean <mdean726@gmail.com>
2023-09-14 15:57:49 -0400

    updates made in class, added get

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,38 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function () {
+const categoryListEle = document.querySelector('#categoryList');
+
+document.addEventListener('DOMContentLoaded', function() {
     console.log("DOM is ready");
 
     const chuckQuote = document.querySelector('#chuckQuote');
 
-    fetch('https://api.chucknorris.io/jokes/random?category=dev')
-        .then(function(response) {
-            return response.json();
-    })
-        .then(function(data) {
-            showQuote(data.value, chuckQuote);
-            return data;
+    const apiUrl = 'https://api.chucknorris.io/jokes/random?category=dev';
+    get(apiUrl).then(function(response) {
+        showQuote(response.value, chuckQuote);
+    });
+
+    const categoriesUrl = 'https://api.chucknorris.io/jokes/categories';
+    get(categoriesUrl).then(function(response) {
+        makeCategoryList(response);
     });
 });
 
 function showQuote(quote, element) {
     element.innerText = quote;
 };
+
+function makeCategoryList(categoryArr) {
+    const selectEle = document.createElement('select');
+    categoryArr.map(function (category) {
+        const option = document.createElement('option');
+        option.value = category;
+        option.text = category;
+        selectEle.appendChild(option);
+    });
+    categoryListEle.append(selectEle);
+};
+
+categoryListEle = addEventListener("submit", function() {
+
+});
